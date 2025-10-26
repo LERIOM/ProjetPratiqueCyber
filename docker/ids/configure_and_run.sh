a557a8358c710eaefeb3018e85099054853d226c
ThoX <antonyhuynh2003@gmail.com>
2025-10-26 04:49:32 -0400

    log display in ids services

--- docker/ids/configure_and_run.sh
+++ docker/ids/configure_and_run.sh
@@ -5,22 +5,16 @@ SNORT_CONF="/etc/snort/snort.conf"
 SNORT_TEMPLATE="/etc/snort/snort.conf.template"
 LOCAL_RULES="/etc/snort/rules/local.rules"
 
-# Syslog distant (défaut : conteneur 'syslog')
-SYSLOG_HOST="${SYSLOG_HOST:-syslog}"
-SYSLOG_PORT="${SYSLOG_PORT:-5514}"
-
-# ------------------- ADAPTATION POUR TEST INTERNE (LOOPBACK) -------------------
-# Fixe l'interface et le réseau au mode Loopback pour garantir la détection sous WSL2
-INTERFACE="lo" 
-HOME_NET="127.0.0.1/8" 
-EXTERNAL_NET="!${HOME_NET}"
+# ------------------- CONFIGURATION RÉSEAU GÉNÉRIQUE -------------------
+INTERFACE="eth0"
+HOME_NET="any"
+EXTERNAL_NET="any"
 # --------------------------------------------------------------------------------
 
 echo "=== Initialisation Snort IDS ==="
-echo "Interface détectée : ${INTERFACE} (Mode Loopback)"
-echo "HOME_NET  : ${HOME_NET}"
+echo "Interface détectée : ${INTERFACE}"
+echo "HOME_NET  : ${HOME_NET}"
 echo "EXTERNAL_NET : ${EXTERNAL_NET}"
-echo "Syslog : ${SYSLOG_HOST}:${SYSLOG_PORT}"
 echo
 
 # Préparation des répertoires
@@ -41,15 +35,13 @@ sed -i "s|^ipvar HOME_NET any|ipvar HOME_NET ${HOME_NET}|" "${SNORT_CONF}"
 sed -i "s|^ipvar EXTERNAL_NET any|ipvar EXTERNAL_NET ${EXTERNAL_NET}|" "${SNORT_CONF}"
 
 # --- 2. Configuration des sorties pour la collecte ---
-# Ajoute les sorties désirées (Syslog pour la collecte, fast pour le debug)
-echo "output alert_syslog: LOG_LOCAL0 LOG_ALERT" >> "${SNORT_CONF}"
 echo "output alert_fast: /var/log/snort/alert" >> "${SNORT_CONF}"
-echo "Sorties de log ajoutées à snort.conf"
+echo "Sortie de log (fichier) ajoutée à snort.conf"
 
 # --- 3. Validation et Lancement ---
 echo "Test de configuration Snort..."
 /usr/sbin/snort -T -c "${SNORT_CONF}" -i "${INTERFACE}"
 
 echo "Lancement de Snort IDS..."
-# Lancement en mode Premier Plan (sans -D) pour voir les alertes dans docker logs -f ids
 exec /usr/sbin/snort -q -i "${INTERFACE}" -c "${SNORT_CONF}"
+

